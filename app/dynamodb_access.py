88dbef4b9e9f0c4f40404af786a4b0d2bb9a17c6
Rasmus <rasmus.adolfsson@student.kyh.se>
2022-03-10 11:16:54 +0100

    inital upload

---
+++ app/dynamodb_access.py
@@ -0,0 +1,15 @@
+import os
+import boto3
+
+
+def get_resource():
+    return boto3.resource('dynamodb',
+                          aws_access_key_id=os.environ['AWS_KEY'],
+                          aws_secret_access_key=os.environ['AWS_SECRET_KEY'],
+                          region_name='eu-north-1')
+
+def get_all_readings():
+    client = get_resource()
+    table = client.Table('customer_counter')
+    response = table.scan()
+    return response['Items']
---
+++ main.py
@@ -0,0 +1,9 @@
+from app.dynamodb_access import get_all_readings
+
+
+def main():
+    print(get_all_readings())
+
+
+if __name__ == '__main__':
+    main()
